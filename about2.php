0e8ca88dd092d792abf36c168e22268c258accfb
Floriane <fl.grosset@gmail.com>
2022-05-01 11:39:01 +0200

    remove old files

--- about2.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-require "classes/AboutPage.php";
-
-$newPage = new AboutPage("/waaw-offline/", $conn);
-$newPage->buildContactPage();
--- js/menu.js
+++
@@ -1,9 +0,0 @@
-function togglemenu() {
-
-    var header = document.getElementById("waaw-header");
-    header.classList.toggle("open");
-    var nav = document.getElementById("nav");
-    nav.classList.toggle("open");
-    var buttonMenu = document.getElementById("buttonMenu");
-    buttonMenu.classList.toggle("toggled");
-}
